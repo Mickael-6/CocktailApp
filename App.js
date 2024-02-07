e07f338b77bfea8862d3b081101e098abed47327
Mickael-6 <mickael.hid@gmail.com>
2024-02-07 18:59:45 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { FlatList, StyleSheet, Text, View,Image, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios'
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const randomLetter = _ => String.fromCharCode(0|Math.random()*26+97)
+  const [datas, setDatas] = useState([]);
+  const [currentPage, setCurrentPage] = useState(97);
+  const getdata = async () => {
+          axios
+          .get(`http://www.thecocktaildb.com/api/json/v1/1/search.php?f=${randomLetter(currentPage)}` )
+          .then(res =>{
+              // setDatas(res.data.drinks)
+           setDatas([...datas, ...res.data.drinks])
+           
+          })
+  }
+
+  const renderItem = ({item}) => {
+    return(
+    <View>
+       <Text  style={styles.titleBlock}> {item.strDrink}</Text> 
+       <Image
+          source={{ uri:item.strDrinkThumb }}
+          style={styles.image}
+        />
     </View>
+    
+    )
+  }
+
+  const renderLoader = () => {
+
+    return (
+      <View>
+     <ActivityIndicator size="large" color ="#aaa"/>
+   </View>
+   
+    )
+    
+  }
+  const loadMoreItem = () => {
+  setCurrentPage(currentPage + 1)
+  }
+
+  useEffect(() => {
+    getdata();
+    
+  }, [currentPage]);
+
+ 
+  return (
+  <FlatList data={datas} 
+  renderItem={renderItem} 
+  ListFooterComponent={renderLoader}
+  onEndReached={loadMoreItem}
+  />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titleBlock : {
+    fontSize: 15,
+    fontWeight: "bold" ,
+   color: "black",
+   marginTop: 50,
+    },
+    image : {
+      width : 150,
+      height : 150,
+    },
 });
